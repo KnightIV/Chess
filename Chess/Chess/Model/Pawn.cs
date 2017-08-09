9bd5a35614f545c4fc76f96c4a259cd3b59c0ec3
Ramon Caballero Villegas <ramon.caballeroiv@gmail.com>
2017-08-09 17:23:42 -0600

    Moved Pawn into Model folder

---
+++ Chess/Chess/Model/Pawn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Model
+{
+    class Pawn : Piece
+    {
+        public Pawn(Coordinate[] threat, Coordinate[] rangeOfMotion, Coordinate initialPos, int playerNum)
+        {
+                
+        }
+    }
+}
