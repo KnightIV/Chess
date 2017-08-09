a8522a53406b15d4362000f3f14c35c938dc7611
William Walsh <wwalsh@student.neumont.edu>
2017-08-09 14:55:16 -0600

    Added Skeleton classes Board.cs Queen.cs **Queen.cs is empty

---
+++ Chess/Chess/Control/Board.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Control
+{
+    class Board
+    {
+        public Dictionary<Position, Space> MyProperty { get; set; }
+
+        public void ResetBoard()
+        {
+
+        }
+
+        public void MovePiece(move Move)
+        {
+
+        }
+    }
+}
